6a3e93ec09392d0c270a38fac0a6d4d75f601379
tetymd <yamada.tetsuya258@gmail.com>
2019-03-01 12:53:36 +0900

    add python-fire

--- edim.py
+++ edim.py
@@ -1,5 +1,22 @@
 from PIL import Image
 import numpy as np
+import fire
+import sys
 
-print(Image.__dir__())
-print(np.__dir__())
+def color(newColor, path):
+    image = Image.open(path)
+    npimage = np.array(image)
+
+    if newColor == 'red':
+        npimage[:, :, (1, 2)] = 0
+    else:
+        print('error!')
+        sys.exit(1)
+
+    image = Image.fromarray(npimage)
+    
+    newImage = './image.png'
+    image.save(newImage)
+
+if __name__ == '__main__':
+    fire.Fire()
